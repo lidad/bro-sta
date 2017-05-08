40ba0f6994b7e37fe8392060eaf5c34296a0ebf5
lidad <497718909@qq.com>
2017-05-08 22:42:03 +0800

    extract slot machine result component

--- src/components/SlotMachine.vue
+++ src/components/SlotMachine.vue
@@ -2,18 +2,7 @@
 <div class="pass-three">
   <ConerStatus/>
   <SlotMachineCast :cast="cast" :rebate="cast * 2" />
-  <div v-if="playing" class="tiger-block frame-box playing">
-    <div class="items"><img v-for="(card, index) in upPlayingCards" class="tiger-item" :class="(index === resultIndex) ? 'b-block' : ''" :src="card" /></div>
-    <div class="items"><img v-for="(card, index) in downPlayingCards" class="tiger-item" :class="(index + 4  === resultIndex) ? 'b-block' : ''" :src="card" /></div>
-    <div class="end-action">
-      <img class="result" :src="resultCard" />
-      <button v-if="endGame" @click="playAgain" class="end t-shadow b-block">再来</button>
-      <button v-else @click="end" class="end t-shadow b-block">停</button>
-    </div>
-  </div>
-  <div v-else class="tiger-block show frame-box">
-    <img v-for="(card, index) in cards" class="tiger-item" :class="(index === selected) ? 'b-block' : ''" :src="card" />
-  </div>
+  <SlotMachineResult/>
   <div class="select-tiger frame-box" :class="{playing}">
     <button class="left btn" @click="selectCard(-1)"><img src="../assets/img/left.png"></button>
     <button class="right btn" @click="selectCard(1)"><img src="../assets/img/right.png"></button>
@@ -40,6 +29,7 @@
 import MapFootLink from './MapFootLink';
 import ConerStatus from './common/ConerStatus';
 import SlotMachineCast from './SlotMachineCast';
+import SlotMachineResult from './SlotMachineResult';
 import Popup from './common/Popup';
 import {
   mapState,
@@ -49,69 +39,17 @@ import {
 
 export default {
   name: 'SlotMachine',
-  beforeUpdate() {
-    if (!this.endShow && this.playing && this.endGame) {
-      let popUpInfo = {};
-      if (this.selected === this.resultIndex) {
-        const rebate = this.cast * 2;
-        popUpInfo = {
-          show: true,
-          title: '红了！老哥稳',
-          tips: rebate + '进账'
-        }
-        this.$store.commit('UpdateBro', {
-          money: this.broStatus.money + rebate,
-        })
-      } else {
-        popUpInfo = {
-          show: true,
-          title: '没赢，老哥不太稳呀',
-          tips: '仍需再接再厉'
-        }
-      }
-      this.endShow = true;
-      this.$store.commit('Show', popUpInfo);
-    }
-  },
-  data() {
-    return {
-      endShow: false
-    }
-  },
   computed: {
     ...mapState({
       odds: state => state.SlotMachineStore.odds,
       playing: state => state.SlotMachineStore.playing,
-      endGame: state => state.SlotMachineStore.ending,
-      cards: state => state.SlotMachineStore.cards,
-      resultIndex: state => state.SlotMachineStore.resultIndex,
-      selected: state => state.SlotMachineStore.tempSelectedIndex,
       broStatus: state => state.BroStore.broStatus,
-      upPlayingCards(state) {
-        const {
-          cards
-        } = state.SlotMachineStore;
-        return cards.slice(0, cards.length / 2);
-      },
-      downPlayingCards(state) {
-        const {
-          cards
-        } = state.SlotMachineStore;
-        return cards.slice(cards.length / 2, cards.length);
-      },
       selectedCard(state) {
         const {
           cards,
           tempSelectedIndex
         } = state.SlotMachineStore;
         return cards[tempSelectedIndex];
-      },
-      resultCard(state) {
-        const {
-          cards,
-          resultIndex
-        } = state.SlotMachineStore;
-        return cards[resultIndex];
       },
       cast(state) {
         const {
@@ -126,11 +64,12 @@ export default {
     MapFootLink,
     ConerStatus,
     SlotMachineCast,
+    SlotMachineResult,
     Popup
   },
   methods: {
-    ...mapMutations(['selectCard', 'adjustOdds', 'playAgain']),
-    ...mapActions(['end', 'beginPlaying']),
+    ...mapMutations(['selectCard', 'adjustOdds']),
+    ...mapActions(['beginPlaying']),
     back() {
       this.$router.back()
     },
@@ -169,52 +108,6 @@ export default {
     height: 100vh;
     background: url('../assets/img/pass_bg.png') left top no-repeat;
     background-size: 100% 100%;
-    .tiger-block {
-        position: fixed;
-        top: 25vh;
-        width: px2rem(180);
-        height: px2rem(120);
-        left: px2rem(88);
-        padding: 0 px2rem(10);
-        &.show {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-around;
-            align-content: space-around;
-        }
-        &.playing {
-            width: px2rem(260);
-            left: px2rem(48);
-            height: px2rem(260);
-            text-align: center;
-            .end-action {
-                margin-top: px2rem(20);
-                line-height: px2rem(120);
-                .result {
-                    width: px2rem(120);
-                    height: px2rem(120);
-                }
-                .end {
-                    margin-left: px2rem(34);
-                    vertical-align: middle;
-                    font-size: px2rem(25);
-                }
-            }
-        }
-        .items {
-            margin-top: px2rem(10);
-            display: flex;
-            justify-content: space-around;
-        }
-        .tiger-item {
-            width: px2rem(40);
-            height: px2rem(40);
-            &.b-block {
-                width: px2rem(38);
-                height: px2rem(38);
-            }
-        }
-    }
     .select-tiger {
         position: fixed;
         left: px2rem(38);
---
+++ src/components/SlotMachineResult.vue
@@ -0,0 +1,137 @@
+<template>
+<div v-if="playing" class="tiger-block frame-box playing">
+  <div class="items"><img v-for="(card, index) in upPlayingCards" class="tiger-item" :class="(index === resultIndex) ? 'b-block' : ''" :src="card" /></div>
+  <div class="items"><img v-for="(card, index) in downPlayingCards" class="tiger-item" :class="(index + 4  === resultIndex) ? 'b-block' : ''" :src="card" /></div>
+  <div class="end-action">
+    <img class="result" :src="resultCard" />
+    <button v-if="endGame" @click="playAgain" class="end t-shadow b-block">再来</button>
+    <button v-else @click="end" class="end t-shadow b-block">停</button>
+  </div>
+</div>
+<div v-else class="tiger-block show frame-box">
+  <img v-for="(card, index) in cards" class="tiger-item" :class="(index === selected) ? 'b-block' : ''" :src="card" />
+</div>
+</template>
+
+<script>
+import {
+  mapState,
+  mapMutations,
+  mapActions
+} from 'vuex';
+export default {
+  name: 'SlotMachineResult',
+  beforeUpdate() {
+    if (!this.endShow && this.playing && this.endGame) {
+      let popUpInfo = {};
+      if (this.selected === this.resultIndex) {
+        const rebate = this.cast * 2;
+        popUpInfo = {
+          show: true,
+          title: '红了！老哥稳',
+          tips: rebate + '进账'
+        }
+        this.$store.commit('UpdateBro', {
+          money: this.broStatus.money + rebate,
+        })
+      } else {
+        popUpInfo = {
+          show: true,
+          title: '没赢，老哥不太稳呀',
+          tips: '仍需再接再厉'
+        }
+      }
+      this.endShow = true;
+      this.$store.commit('Show', popUpInfo);
+    }
+  },
+  data() {
+    return {
+      endShow: false
+    }
+  },
+  computed: {
+    ...mapState({
+      playing: state => state.SlotMachineStore.playing,
+      endGame: state => state.SlotMachineStore.ending,
+      cards: state => state.SlotMachineStore.cards,
+      resultIndex: state => state.SlotMachineStore.resultIndex,
+      selected: state => state.SlotMachineStore.tempSelectedIndex,
+      broStatus: state => state.BroStore.broStatus,
+      upPlayingCards(state) {
+        const {
+          cards
+        } = state.SlotMachineStore;
+        return cards.slice(0, cards.length / 2);
+      },
+      downPlayingCards(state) {
+        const {
+          cards
+        } = state.SlotMachineStore;
+        return cards.slice(cards.length / 2, cards.length);
+      },
+      resultCard(state) {
+        const {
+          cards,
+          resultIndex
+        } = state.SlotMachineStore;
+        return cards[resultIndex];
+      }
+    })
+  },
+  methods: {
+    ...mapMutations(['playAgain']),
+    ...mapActions(['end']),
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/rem.scss";
+.tiger-block {
+    position: fixed;
+    top: 25vh;
+    width: px2rem(180);
+    height: px2rem(120);
+    left: px2rem(88);
+    padding: 0 px2rem(10);
+    &.show {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-content: space-around;
+    }
+    &.playing {
+        width: px2rem(260);
+        left: px2rem(48);
+        height: px2rem(260);
+        text-align: center;
+        .end-action {
+            margin-top: px2rem(20);
+            line-height: px2rem(120);
+            .result {
+                width: px2rem(120);
+                height: px2rem(120);
+            }
+            .end {
+                margin-left: px2rem(34);
+                vertical-align: middle;
+                font-size: px2rem(25);
+            }
+        }
+    }
+    .items {
+        margin-top: px2rem(10);
+        display: flex;
+        justify-content: space-around;
+    }
+    .tiger-item {
+        width: px2rem(40);
+        height: px2rem(40);
+        &.b-block {
+            width: px2rem(38);
+            height: px2rem(38);
+        }
+    }
+}
+</style>
