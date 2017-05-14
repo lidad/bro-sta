98500dcfce63a67d2c95d65cccdfde6f1476a3a4
lidad <497718909@qq.com>
2017-05-14 17:33:44 +0800

    midify select dish and add dish to OffcialHotel

--- src/components/OffcialHotel.vue
+++ src/components/OffcialHotel.vue
@@ -1,15 +1,36 @@
 <template>
 <div class="offcial-hotel">
   <ConerStatus/>
+  <section class="dishes">
+  </section>
   <MapFootLink/>
 </div>
 </template>
 <script>
+import {
+  mapState,
+  mapMutations
+} from 'vuex';
+
 import ConerStatus from './common/ConerStatus';
 import MapFootLink from './MapFootLink';
 
 export default {
   name: 'OffcialHotel',
+  computed: {
+    ...mapState({
+        selectdedDish(state) {
+          const {
+            dishes,
+            selectdedDishIndedx
+          } = state.HotelStore;
+          return dishes[selectdedDishIndedx];
+        }
+    }),
+},
+methods: {
+    ...mapMutations(['selectDish']),
+  },
   components: {
     ConerStatus,
     MapFootLink
---
+++ src/store/HotelStore.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+cnost DISH = [
+  {
+    id: 'k13hkr31413',
+    name: '飘香拌面',
+    info: '就是麻酱加点面条',
+    fee: 5,
+    effect: 5
+  }, {
+    id: '3rf2f34g343',
+    name: '蒸饺',
+    info: '蒸饺，就是那个蒸饺，篓子上摆了几个饺子，中间一碟醋',
+    fee: 5,
+    effect: 5
+  }, {
+    id: 'ri2h3jhfsd',
+    name: '鸭腿饭',
+    info: '官方大酒店主打，米饭上托举着硕大的鸭腿，有饭有肉',
+    fee: 14,
+    effect: 10
+  }, {
+    id: 'hkjhk32h4123kj',
+    name: '大排饭',
+    info: '相比鸭腿饭仁者见仁智者见智，差不多的档次配置',
+    fee: 14,
+    effect: 10
+  }, {
+    id: '1ui5hi1hr3',
+    name: '至尊商务套餐',
+    info: '各种浇头随意搭配，脑海里想象一下沙县里的那个堆满卤味的盆，嗯，就是那样',
+    fee: 25,
+    effect: 20
+  }
+]
+
+const state = {
+  dishes: DISH,
+  selectdedDishIndedx: 0
+}
+
+const mutations = {
+  selectDish(state, dir) {
+    const {selectdedDishIndedx, dishes} = state;
+    let index = selectdedDishIndedx + dir;
+    if (index < 0)
+      index = dishes.length - 1;
+    if (index >= dishes.length)
+      index = 0;
+    state.selectdedDishIndedx = index;
+  }
+}
+
+export default {
+  state,
+  mutations
+}
